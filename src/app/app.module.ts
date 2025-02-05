1f07b78ba2645052b976a7a539c1ca4fbb20cf45
yamileth caldera <yamilethcaldera@gmail.com>
2025-02-04 22:49:41 -0300

    guardando mis cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { NavComponent } from './components/nav/nav.component';
-import { CardProyectosComponent } from './components/card-proyectos/card-proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutmeComponent } from './contenido/aboutme/aboutme.component';
-import { HabilidadesComponent } from './contenido/habilidades/habilidades.component';
-import { FormacionComponent } from './contenido/formacion/formacion.component';
 import { ProyectosComponent } from './contenido/proyectos/proyectos.component';
-import { ContactoComponent } from './contenido/contacto/contacto.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent,
-    CardProyectosComponent,
     FooterComponent,
     HomeComponent,
     AboutmeComponent,
-    HabilidadesComponent,
-    FormacionComponent,
     ProyectosComponent,
-    ContactoComponent
+    PresentacionComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
